459446a33e482147e506809c58e3c1c0691cf348
kyle-shk <guqtls85@naver.com>
2022-12-16 16:55:09 +0900

    정리

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by 김신협 on 2022/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
